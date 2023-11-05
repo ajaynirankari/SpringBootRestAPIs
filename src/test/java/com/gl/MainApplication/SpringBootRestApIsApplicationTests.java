0ab74f7b02a970bf8be9503a08a7d1b810156064
Ajay.Nirankari <ajaykumarnirankarigl@gmail.com>
2023-11-05 13:05:48 +0530

    first commit

---
+++ src/test/java/com/gl/MainApplication/SpringBootRestApIsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gl.MainApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestApIsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
